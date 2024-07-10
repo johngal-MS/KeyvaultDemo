06c10f8df47cc285e22d7d6941777f9d15b70897
John Galindo <john_galindo@hotmail.com>
2024-07-10 09:23:26 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//using Azure.Identity;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        string keyVaultName = "johngal-fbi";
+        string secretName = "secret1";
+
+        var client = new SecretClient(new Uri($"https://{keyVaultName}.vault.azure.net/"), new DefaultAzureCredential());
+
+        KeyVaultSecret secret = client.GetSecret(secretName);
+
+        Console.WriteLine($"Secret Name: {secret.Name}");
+        Console.WriteLine($"Secret Value: {secret.Value}");
+    }
+}
